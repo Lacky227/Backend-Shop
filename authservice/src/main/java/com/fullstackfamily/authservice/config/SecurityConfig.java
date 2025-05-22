14b9554e31fa2acbbcde4ba33ee3bfb51b1386b7
Lacky227 <tishko.artyr@gmail.com>
2025-05-22 14:45:31 +0300

    refactor(config-SecurityConfig): remove an imports and security filter chain

--- authservice/src/main/java/com/fullstackfamily/authservice/config/SecurityConfig.java
+++ authservice/src/main/java/com/fullstackfamily/authservice/config/SecurityConfig.java
@@ -2,26 +2,11 @@ package com.fullstackfamily.authservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> {
-                    auth.anyRequest().authenticated();
-                })
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .httpBasic(HttpBasicConfigurer::disable)
-                .build();
-    }
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
