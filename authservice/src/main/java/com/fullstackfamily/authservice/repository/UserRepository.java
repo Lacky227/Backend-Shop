b6be1244a8d6139d3865e0877db118cc390ae4d9
artem <artemstupnickij8@gmail.com>
2025-05-22 23:19:39 +0300

    feat: update user repository

--- authservice/src/main/java/com/fullstackfamily/authservice/repository/UserRepository.java
+++ authservice/src/main/java/com/fullstackfamily/authservice/repository/UserRepository.java
@@ -11,9 +11,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
 
+    Optional<User> findByEmail(String email);
+
     boolean existsByEmail(String email);
-    
+
     boolean existsByUsername(String username);
-
-
 }
