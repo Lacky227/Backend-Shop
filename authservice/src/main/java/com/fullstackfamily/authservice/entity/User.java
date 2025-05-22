a863d848dc8ea012c3f8bfb926f84a830113b9dc
artem <artemstupnickij8@gmail.com>
2025-05-22 16:08:15 +0300

    feat: create user entity

---
+++ authservice/src/main/java/com/fullstackfamily/authservice/entity/User.java
@@ -0,0 +1,33 @@
+package com.fullstackfamily.authservice.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table (name = "users")
+
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role = "ROLE_USER";
+
+
+}
