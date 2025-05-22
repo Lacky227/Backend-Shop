e09a14b21b76c89c7be0e5f938e6a2f9ed45bfb6
Lacky227 <tishko.artyr@gmail.com>
2025-05-22 14:12:17 +0300

    feat(service-JwtService): create JWTService for generate token and extract all claims

---
+++ common-jwt/src/main/java/com/fullstackfamily/commonjwt/service/JwtService.java
@@ -0,0 +1,39 @@
+package com.fullstackfamily.commonjwt.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    private static final String SECRET_KEY = "3F1F6A0A2B4E6F8C9D0E11223344556677889900AABBCCDDEEFF001122334455";
+
+    public String generateToken(String username, String role) {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("role", role)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
