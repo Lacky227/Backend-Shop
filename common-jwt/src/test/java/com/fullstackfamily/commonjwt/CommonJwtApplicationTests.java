fae2d201a58caa9d7be63679bfc34564d17892c9
Lacky227 <tishko.artyr@gmail.com>
2025-05-22 14:05:40 +0300

    feat: create a common-jwt service for generating JWT token and extract all claims

---
+++ common-jwt/src/test/java/com/fullstackfamily/commonjwt/CommonJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstackfamily.commonjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
