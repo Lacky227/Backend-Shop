697b6f2cd7e5c08093837a015e1548182f8291a9
Lacky227 <tishko.artyr@gmail.com>
2025-05-27 01:20:16 +0300

    feat(config-GatewayConfig): add route configuration to auth-service

--- apigateway/src/main/java/com/fullstackfamily/apigateway/config/GatewayConfig.java
+++ apigateway/src/main/java/com/fullstackfamily/apigateway/config/GatewayConfig.java
@@ -1,7 +1,22 @@
 package com.fullstackfamily.apigateway.config;
 
+import com.fullstackfamily.apigateway.filter.JwtFilter;
+import lombok.AllArgsConstructor;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@AllArgsConstructor
 public class GatewayConfig {
+    private final JwtFilter jwtFilter;
+
+    RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("auth-service", r -> r
+                                .path("/auth/**")
+                                .uri("http://localhost:8081")
+                        )
+                .build();
+    }
 }
