d60204f7dbf94d9e060d94ff2137e4947e6a67bd
Lacky227 <tishko.artyr@gmail.com>
2025-05-27 23:47:03 +0300

    feat(api-gateway-config.GatewayConfig): I don`t know

--- apigateway/src/main/java/com/fullstackfamily/apigateway/config/GatewayConfig.java
+++ apigateway/src/main/java/com/fullstackfamily/apigateway/config/GatewayConfig.java
@@ -4,6 +4,7 @@ import com.fullstackfamily.apigateway.filter.JwtFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -11,12 +12,12 @@ import org.springframework.context.annotation.Configuration;
 public class GatewayConfig {
     private final JwtFilter jwtFilter;
 
+    @Bean
     RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("auth-service", r -> r
                                 .path("/auth/**")
-                                .uri("http://localhost:8081")
-                        )
+                                .uri("http://localhost:8081/"))
                 .build();
     }
 }
