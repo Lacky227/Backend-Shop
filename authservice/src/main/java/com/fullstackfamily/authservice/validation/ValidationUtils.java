2cb22381efbe00b4d45c98f35d3479af71a83b9e
Lacky227 <tishko.artyr@gmail.com>
2025-06-06 14:16:24 +0300

    refactor(validation-ValidationUtils): rename Valid to ValidationUtils with @UtilityClass

--- authservice/src/main/java/com/fullstackfamily/authservice/validation/ValidationUtils.java
+++ authservice/src/main/java/com/fullstackfamily/authservice/validation/ValidationUtils.java
@@ -1,18 +1,21 @@
 package com.fullstackfamily.authservice.validation;
 
-public class Valid {
+import lombok.experimental.UtilityClass;
 
-    public static boolean emailInvalid(String email) {
+@UtilityClass
+public class ValidationUtils {
+
+    public boolean emailInvalid(String email) {
         return email == null || email.isEmpty() ||
                 !email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
     }
 
-    public static boolean passwordInvalid(String password) {
+    public boolean passwordInvalid(String password) {
         return password == null || password.isEmpty() ||
                 password.length() > 50 ||
                 password.matches(".*[а-яА-ЯїЇєЄіІґҐ].*");
     }
-    public static boolean usernameInvalid(String username) {
+    public boolean usernameInvalid(String username) {
         return username == null || username.isEmpty() ||
                 !username.matches("^[a-zA-Z0-9!@#$%^&*()_+\\-=<>?.:,]{5,15}$");
     }
