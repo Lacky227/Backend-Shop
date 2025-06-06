f009260014e88cce5cb0ac82e0e9f4e77e278200
Lacky227 <tishko.artyr@gmail.com>
2025-06-06 13:50:03 +0300

    refactor(validation-Valid): put methods in static

--- authservice/src/main/java/com/fullstackfamily/authservice/validation/Valid.java
+++ authservice/src/main/java/com/fullstackfamily/authservice/validation/Valid.java
@@ -2,17 +2,17 @@ package com.fullstackfamily.authservice.validation;
 
 public class Valid {
 
-    public boolean emailInvalid(String email) {
+    public static boolean emailInvalid(String email) {
         return email == null || email.isEmpty() ||
                 !email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
     }
 
-    public boolean passwordInvalid(String password) {
+    public static boolean passwordInvalid(String password) {
         return password == null || password.isEmpty() ||
                 password.length() > 50 ||
                 password.matches(".*[а-яА-ЯїЇєЄіІґҐ].*");
     }
-    public boolean usernameRegex(String username) {
+    public static boolean usernameRegex(String username) {
         return username == null || username.isEmpty() ||
                 !username.matches("^[a-zA-Z0-9!@#$%^&*()_+\\-=<>?.:,]{5,15}$");
     }
