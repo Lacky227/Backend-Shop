60e210a16812f2cf3c1125f8db3f91ecbc3c1212
Lacky227 <tishko.artyr@gmail.com>
2025-06-10 20:37:15 +0300

    feat: create NotificationService

---
+++ notificationservice/src/test/java/com/fullstackfamily/notificationservice/NotificationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstackfamily.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
