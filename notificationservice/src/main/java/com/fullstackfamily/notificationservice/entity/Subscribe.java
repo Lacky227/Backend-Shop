86a5754921fcbc933c468baa2af5e889eaf6f4cd
Lacky227 <tishko.artyr@gmail.com>
2025-06-10 20:49:01 +0300

    feat(entity-Subscribe): create entity for subscribers

---
+++ notificationservice/src/main/java/com/fullstackfamily/notificationservice/entity/Subscribe.java
@@ -0,0 +1,14 @@
+package com.fullstackfamily.notificationservice.entity;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collation = "subscribers")
+public class Subscribe {
+    @Id
+    private String id;
+    private String email;
+    private boolean isSubscribed;
+}
