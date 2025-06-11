24f134c6de32dc34148a928687254403a0087380
Lacky227 <tishko.artyr@gmail.com>
2025-06-11 15:39:33 +0300

    refactor(entity-Subscriber): rechange collection

--- notificationservice/src/main/java/com/fullstackfamily/notificationservice/entity/Subscriber.java
+++ notificationservice/src/main/java/com/fullstackfamily/notificationservice/entity/Subscriber.java
@@ -5,7 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document(collation = "subscribers")
+@Document(collection = "subscribers")
 public class Subscriber {
     @Id
     private String id;
