8f14e434ee8b62a094d80e82a0a801626c4ad802
Lacky227 <tishko.artyr@gmail.com>
2025-06-11 14:33:19 +0300

    refactor(validation-ValidationUtils): remove excess

--- notificationservice/src/main/java/com/fullstackfamily/notificationservice/validation/ValidationUtils.java
+++ notificationservice/src/main/java/com/fullstackfamily/notificationservice/validation/ValidationUtils.java
@@ -2,11 +2,9 @@ package com.fullstackfamily.notificationservice.validation;
 
 import lombok.experimental.UtilityClass;
 
-import java.util.regex.Pattern;
-
 @UtilityClass
 public class ValidationUtils {
-    private final String EMAIL_PATTERN = "^(?=.{10,50}$)[A-Za-z0-9](?!.*[._+-]{2})[A-Za-z0-9+_.-]*[A-Za-z0-9]@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*\\.(com|org|ua|net)$";;
+    private final String EMAIL_PATTERN = "^(?=.{10,50}$)[A-Za-z0-9](?!.*[._+-]{2})[A-Za-z0-9+_.-]*[A-Za-z0-9]@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*\\.(com|org|ua|net)$";
 
     public boolean validateEmail(String email) {
         if (email == null || email.isEmpty()) return true;
