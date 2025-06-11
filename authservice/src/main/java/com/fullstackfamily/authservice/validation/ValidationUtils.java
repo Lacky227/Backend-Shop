ab0a704cc340be1d75467e5bb1f9749eab79964d
Lacky227 <tishko.artyr@gmail.com>
2025-06-11 16:47:52 +0300

    refactor(validation-ValidationUtils): upgrade method for valid email and password

--- authservice/src/main/java/com/fullstackfamily/authservice/validation/ValidationUtils.java
+++ authservice/src/main/java/com/fullstackfamily/authservice/validation/ValidationUtils.java
@@ -4,16 +4,21 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class ValidationUtils {
+    private final String EMAIL_PATTERN =
+            "^(?=.{10,50}$)[A-Za-z0-9](?!.*[._+-]{2})[A-Za-z0-9+_.-]*[A-Za-z0-9]@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*\\.(com|org|ua|net)$";
+    private final String PASSWORD_PATTERN =
+            "^(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+=\\-\\[\\]{};':\"\\\\|,.<>/?]).{8,30}$";
 
     public boolean emailInvalid(String email) {
-        return email == null || email.isEmpty() ||
-                !email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
+        if (email == null || email.isEmpty()) return true;
+        return !email.matches(EMAIL_PATTERN);
     }
 
     public boolean passwordInvalid(String password) {
-        return password == null || password.isEmpty() ||
-                password.length() > 50 ||
-                password.matches(".*[а-яА-ЯїЇєЄіІґҐ].*");
+        if (password == null || password.isEmpty()) return true;
+        if (password.length() < 8 || password.length() > 30) return true;
+        if (password.matches(".*[а-яА-ЯїЇєЄіІґҐ].*")) return true;
+        return !password.matches(PASSWORD_PATTERN);
     }
     public boolean usernameInvalid(String username) {
         return username == null || username.isEmpty() ||
