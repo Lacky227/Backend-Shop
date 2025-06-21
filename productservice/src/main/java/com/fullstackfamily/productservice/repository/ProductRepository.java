c5ea442808ecc1c6359f71fd8d07840935fc5118
Lacky227 <tishko.artyr@gmail.com>
2025-06-21 19:03:35 +0300

    feat(repository-ProductRepository): create repository

---
+++ productservice/src/main/java/com/fullstackfamily/productservice/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.fullstackfamily.productservice.repository;
+
+import com.fullstackfamily.productservice.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
