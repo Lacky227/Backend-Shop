27f5268cab5440ec721f9afa8af6ab07e440dbdf
Lacky227 <tishko.artyr@gmail.com>
2025-06-23 19:02:01 +0300

    feat(dto-ProductInfoRequest): create dto class for product input data

---
+++ productservice/src/main/java/com/fullstackfamily/productservice/dto/ProductInfoRequest.java
@@ -0,0 +1,37 @@
+package com.fullstackfamily.productservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class ProductInfoRequest {
+    private String name;
+
+    private String brand;
+
+    private String gender;
+
+    private String category;
+
+    private BigDecimal price;
+
+    private BigDecimal oldPrice;
+
+    private Boolean hasdiscount;
+
+    private List<String> image;
+
+    private List<String> sizes;
+
+    private String color;
+
+    private String season;
+
+    private String description;
+
+    private String material;
+}
