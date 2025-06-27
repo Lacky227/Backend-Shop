256822efac0d3bd7815c5b2cf913e8160ed0787d
Lacky227 <tishko.artyr@gmail.com>
2025-06-27 22:58:20 +0300

    feat(config-StaticResourceConfig): change variable

--- productservice/src/main/java/com/fullstackfamily/productservice/config/StaticResourceConfig.java
+++ productservice/src/main/java/com/fullstackfamily/productservice/config/StaticResourceConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class StaticResourceConfig implements WebMvcConfigurer {
     @Value("${local.name.directory}")
-    private static String RESOURCE_LOCATION;
+    private String RESOURCE_LOCATION;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
