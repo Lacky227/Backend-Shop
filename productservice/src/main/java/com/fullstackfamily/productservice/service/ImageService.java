7340305f6f8272b7c2968751bb389e92fcc7bca3
Lacky227 <tishko.artyr@gmail.com>
2025-07-08 19:35:09 +0300

    refactor(service-ImageService): update the service to use cloudinary and add new format names photo

--- productservice/src/main/java/com/fullstackfamily/productservice/service/ImageService.java
+++ productservice/src/main/java/com/fullstackfamily/productservice/service/ImageService.java
@@ -1,35 +1,31 @@
 package com.fullstackfamily.productservice.service;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import com.fullstackfamily.productservice.dto.ApiResponse;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Map;
 
 @Service
+@AllArgsConstructor
 public class ImageService {
-    @Value("${local.name.directory}")
-    private String RESOURCE_LOCATION;
+    private final Cloudinary cloudinary;
 
     public ResponseEntity<ApiResponse> saveImage(MultipartFile file) {
         try {
             String originalFilename = file.getOriginalFilename();
-            if (file.getSize() == 0 || originalFilename == null || !originalFilename.matches("TSH\\d+-\\d+\\.(png|jpg|jpeg)")) {
+            if (file.getSize() == 0 || originalFilename == null || !originalFilename.matches("(TSH|TRS|SWT|OUT)\\d+-\\d+\\.(png|jpg|jpeg)")) {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse("Не вірний формат файлу або файл не існує"));
             }
-            Path uploadDir = Paths.get(RESOURCE_LOCATION);
-            if (!Files.exists(uploadDir)) {
-                Files.createDirectories(uploadDir);
-            }
-            Path filePath = uploadDir.resolve(originalFilename);
-            Files.write(filePath, file.getBytes());
-            return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse("/images/" + originalFilename));
+            Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
+            String imageUrl = uploadResult.get("secure_url").toString();
+            return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse(imageUrl));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
