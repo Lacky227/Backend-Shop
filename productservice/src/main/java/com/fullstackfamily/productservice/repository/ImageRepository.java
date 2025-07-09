9f402aba1c074d81b173b6a25ed323bcf9a5372d
Lacky227 <tishko.artyr@gmail.com>
2025-07-09 18:55:49 +0300

    feat(repository-ImageRepository): create a repository for images

---
+++ productservice/src/main/java/com/fullstackfamily/productservice/repository/ImageRepository.java
@@ -0,0 +1,12 @@
+package com.fullstackfamily.productservice.repository;
+
+import com.fullstackfamily.productservice.entity.Images;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Images, Long> {
+    List<Images> findAllByProductSku(String sku);
+}
