195e04f5bdc28af27eceb24cc81f1856466d9e64
Lacky227 <tishko.artyr@gmail.com>
2025-07-10 21:30:20 +0300

    refactor(service-ImageService): change method for save image

--- productservice/src/main/java/com/fullstackfamily/productservice/service/ImageService.java
+++ productservice/src/main/java/com/fullstackfamily/productservice/service/ImageService.java
@@ -16,6 +16,8 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 @AllArgsConstructor
@@ -27,23 +29,46 @@ public class ImageService {
     public ResponseEntity<ApiResponse> saveImage(MultipartFile file) {
         try {
             String originalFilename = file.getOriginalFilename();
-            if (file.getSize() == 0 || originalFilename == null || !originalFilename.matches("(TSH|TRS|SWT|OUT)\\d+-\\d+\\.(png|jpg|jpeg)")) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse("Не вірний формат файлу або файл не існує"));
+            if (file.getSize() == 0 || originalFilename == null) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body(new ApiResponse("Не вірний формат файлу або файл не існує"));
             }
-            String sku = originalFilename.substring(0, originalFilename.indexOf("-"));
+
+            Pattern pattern = Pattern.compile("((TSH|TRS|SWT|OUT)\\d+)-\\d+\\.(png|jpg|jpeg)", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(originalFilename);
+            if (!matcher.matches()) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body(new ApiResponse("Невірний формат імені файлу"));
+            }
+
+            String sku = matcher.group(1);
+            String baseName = originalFilename.substring(0, originalFilename.lastIndexOf('.'));
+
             Optional<Product> product = productRepository.findBySku(sku);
             if (product.isEmpty()) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse("Товар з SKU '" + sku + "' не знайдено"));
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body(new ApiResponse("Товар з SKU '" + sku + "' не знайдено"));
+            } else if (imageRepository.existsByOriginalName(baseName)){
+                return ResponseEntity.badRequest()
+                        .body(new ApiResponse("Таке фото вже існує"));
             }
 
             Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
             String imageUrl = uploadResult.get("secure_url").toString();
+            if (imageUrl == null) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .body(new ApiResponse("Не вдалося отримати URL завантаженого зображення"));
+            }
+
             Images image = new Images();
-            image.setOriginalName(originalFilename);
+            image.setOriginalName(baseName);
             image.setUrl(imageUrl);
             image.setProduct(product.get());
             imageRepository.save(image);
-            return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse("Фото збережено"));
+
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(new ApiResponse("Фото збережено"));
+
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(new ApiResponse("Помилка при завантаженні фото"));
