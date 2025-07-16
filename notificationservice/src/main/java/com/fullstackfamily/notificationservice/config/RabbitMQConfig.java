293f8f1571505b47abe8b1eaab69672b4b2e89dd
Lacky227 <tishko.artyr@gmail.com>
2025-07-16 20:36:02 +0300

    refactor: update configuration RabbitMQ

--- notificationservice/src/main/java/com/fullstackfamily/notificationservice/config/RabbitMQConfig.java
+++ notificationservice/src/main/java/com/fullstackfamily/notificationservice/config/RabbitMQConfig.java
@@ -1,15 +1,32 @@
 package com.fullstackfamily.notificationservice.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableRabbit
 public class RabbitMQConfig {
     public static final String QUEUE_NAME = "auth-to-notif";
+    public static final String EXCHANGE_NAME = "auth-exchange";
+    public static final String ROUTING_KEY = "auth.to.notif";
 
     @Bean
     public Queue queue() {
         return new Queue(QUEUE_NAME, true);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
     }
 }
