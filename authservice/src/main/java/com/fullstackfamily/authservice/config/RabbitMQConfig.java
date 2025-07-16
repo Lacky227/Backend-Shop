7da4fe421a9acade4129de877836b0521b499fa6
Lacky227 <tishko.artyr@gmail.com>
2025-07-16 11:41:55 +0300

    feat(config-RabbitMQConfig): add RabbitMQ configuration

---
+++ authservice/src/main/java/com/fullstackfamily/authservice/config/RabbitMQConfig.java
@@ -0,0 +1,22 @@
+package com.fullstackfamily.authservice.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    private static final String QUEUE_NAME = "auth-to-notif";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME, true);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        return new RabbitTemplate(connectionFactory);
+    }
+}
