974ab975f410580ea98fea73340101b8592bb341
BellamyCeleman <buzzardd239@gmail.com>
2025-07-16 12:25:47 +0200

    corrections to productservice swagger

--- productservice/src/main/java/com/fullstackfamily/productservice/controller/ImageController.java
+++ productservice/src/main/java/com/fullstackfamily/productservice/controller/ImageController.java
@@ -2,6 +2,11 @@ package com.fullstackfamily.productservice.controller;
 
 import com.fullstackfamily.productservice.dto.ApiResponse;
 import com.fullstackfamily.productservice.service.ImageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,9 +18,36 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/api/files")
 @AllArgsConstructor
+@Tag(name = "Завантаження зображення", description = "REST-контролер для роботи із завантаженням зображень")
 public class ImageController {
     private final ImageService imageService;
 
+    @Operation(
+            summary = "Завантаження зображення",
+            description = "Приймає файл зображення і зберігає його на сервері"
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "201",
+                    description = "Фото збережено",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "400",
+                    description = "Некоректний файл або ім'я файлу, або фото вже існує",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "404",
+                    description = "Товар не знайдено",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "500",
+                    description = "Внутрішня помилка сервера",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))
+            )
+    })
     @PostMapping("/save-image")
     public ResponseEntity<ApiResponse> saveImage(@RequestParam("file") MultipartFile file) {
         return imageService.saveImage(file);
