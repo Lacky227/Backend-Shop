4a66e70fb53b06124b96cf8c8484b9e26bd8217d
Lacky227 <tishko.artyr@gmail.com>
2025-07-17 22:34:48 +0300

    feat(config-RabbitMQConfig): rename and add a new queue

--- authservice/src/main/java/com/fullstackfamily/authservice/config/RabbitMQConfig.java
+++ authservice/src/main/java/com/fullstackfamily/authservice/config/RabbitMQConfig.java
@@ -11,13 +11,20 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-    public static final String QUEUE_NAME = "auth-to-notif";
+    public static final String SUBSCRIPTION_QUEUE_NAME = "auth-to-notif";
+    public static final String RESET_QUEUE_NAME = "reset-password-queue";
     public static final String EXCHANGE_NAME = "auth-exchange";
-    public static final String ROUTING_KEY = "auth.to.notif";
+    public static final String SUBSCRIPTION_ROUTING_KEY = "auth.to.notif";
+    public static final String RESET_ROUTING_KEY = "auth.to.reset";
 
     @Bean
-    public Queue queue() {
-        return new Queue(QUEUE_NAME, true);
+    public Queue subscriptionQueue() {
+        return new Queue(SUBSCRIPTION_QUEUE_NAME, true);
+    }
+
+    @Bean
+    public Queue resetQueue() {
+        return new Queue(RESET_QUEUE_NAME, true);
     }
 
     @Bean
@@ -26,8 +33,13 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public Binding binding(Queue queue, DirectExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    public Binding subscriptionBinding(Queue subscriptionQueue, DirectExchange exchange) {
+        return BindingBuilder.bind(subscriptionQueue).to(exchange).with(SUBSCRIPTION_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding resetBinding(Queue resetQueue, DirectExchange exchange) {
+        return BindingBuilder.bind(resetQueue).to(exchange).with(RESET_ROUTING_KEY);
     }
 
     @Bean
