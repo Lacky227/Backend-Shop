044cbb4189b0ef2b120fc4fad3f5acfc0b895554
Lacky227 <tishko.artyr@gmail.com>
2025-07-18 13:53:26 +0000

    refactor(service-JwtService): upgrade service

--- notificationservice/src/main/java/com/fullstackfamily/notificationservice/service/JwtService.java
+++ notificationservice/src/main/java/com/fullstackfamily/notificationservice/service/JwtService.java
@@ -1,7 +1,6 @@
 package com.fullstackfamily.notificationservice.service;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.AllArgsConstructor;
@@ -17,20 +16,51 @@ import java.util.Date;
 public class JwtService {
     @Value("${secret.key}")
     private String SECRET_KEY;
+    private static final long UNSUBSCRIBE_TOKEN_VALIDITY = 0;
+    private static final long FORGOT_PASSWORD_TOKEN_VALIDITY = 15 * 60 * 1000;
 
-    public String generateTokenUnsubscribe(String email) {
-        return Jwts.builder()
-                .setSubject(email)
-                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
-                .compact();
-    }
-    public String generateTokenForgotPassword(String email) {
+    public String generateToken(String email, boolean isExpirable) {
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 15))
+                .setExpiration(isExpirable ? new Date(System.currentTimeMillis() + FORGOT_PASSWORD_TOKEN_VALIDITY) : null)
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public String generateTokenUnsubscribe(String email) {
+        return generateToken(email, false);
+    }
+
+    public String generateTokenForgotPassword(String email) {
+        return generateToken(email, true);
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parserBuilder()
+                    .setSigningKey(getSignInKey())
+                    .build()
+                    .parseClaimsJws(token);
+
+            // Перевірка терміну дії, якщо він заданий
+            Date expiration = claims.getBody().getExpiration();
+            if (expiration != null && expiration.before(new Date())) {
+                return false;
+            }
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
     }
 
     private Key getSignInKey() {
