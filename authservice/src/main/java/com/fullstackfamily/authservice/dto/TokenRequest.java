737adbdb49caf0b70272b9ef8b6aa3c5c9c80dc4
Lacky227 <tishko.artyr@gmail.com>
2025-07-18 15:52:36 +0000

    feat: add new variable

--- authservice/src/main/java/com/fullstackfamily/authservice/dto/TokenRequest.java
+++ authservice/src/main/java/com/fullstackfamily/authservice/dto/TokenRequest.java
@@ -1,8 +1,10 @@
 package com.fullstackfamily.authservice.dto;
 
+import io.jsonwebtoken.Claims;
 import lombok.Data;
 
 @Data
 public class TokenRequest {
     private String token;
+    private String newPassword;
 }
