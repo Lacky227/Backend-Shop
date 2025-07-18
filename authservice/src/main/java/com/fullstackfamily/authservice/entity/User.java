704251704a1e7f3549b96fbe397bfe5928f64888
Lacky227 <tishko.artyr@gmail.com>
2025-07-18 15:05:56 +0000

    refactor: remove unique for first name

--- authservice/src/main/java/com/fullstackfamily/authservice/entity/User.java
+++ authservice/src/main/java/com/fullstackfamily/authservice/entity/User.java
@@ -10,7 +10,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false)
     private String firstName;
 
     private String lastName;
