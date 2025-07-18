00b5f1221dda51986a493a9879b08981564500c0
Lacky227 <tishko.artyr@gmail.com>
2025-07-18 13:19:07 +0000

    feat(service-JwtService): add class for management jwt

---
+++ notificationservice/src/main/java/com/fullstackfamily/notificationservice/service/JwtService.java
@@ -0,0 +1,40 @@
+package com.fullstackfamily.notificationservice.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+    @Value("${secret.key}")
+    private String SECRET_KEY;
+
+    public String generateTokenUnsubscribe(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+    public String generateTokenForgotPassword(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 15))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
