ff0ed3161fdca0b491a1c9e5b37e3caeee3a1a68
Lacky227 <tishko.artyr@gmail.com>
2025-07-24 21:27:10 +0300

    feat: add JpaSpecificationExecutor

--- productservice/src/main/java/com/fullstackfamily/productservice/repository/ProductRepository.java
+++ productservice/src/main/java/com/fullstackfamily/productservice/repository/ProductRepository.java
@@ -2,11 +2,12 @@ package com.fullstackfamily.productservice.repository;
 
 import com.fullstackfamily.productservice.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     Optional<Product> findBySku(String sku);
 }
