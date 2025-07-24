f8ce906f0d0c9648867ca59e0bc4cb1afabe7ffb
Lacky227 <tishko.artyr@gmail.com>
2025-07-24 19:58:14 +0300

    feat: update swagger doc

--- productservice/src/main/java/com/fullstackfamily/productservice/dto/APIResponse.java
+++ productservice/src/main/java/com/fullstackfamily/productservice/dto/APIResponse.java
@@ -1,10 +1,16 @@
 package com.fullstackfamily.productservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Schema(description = "Універсальна відповідь API з повідомленням про результат операції")
 public class APIResponse {
+    @Schema(
+            description = "Текстове повідомлення про результат виконання запиту",
+            example = "Операція виконана успішно"
+    )
     private String message;
 }
