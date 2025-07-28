58db75529570988c8b22533d43edae9199e0181c
Lacky227 <tishko.artyr@gmail.com>
2025-07-28 15:21:29 +0300

    refactor: update method to use composite key for image search

--- productservice/src/main/java/com/fullstackfamily/productservice/repository/ImageRepository.java
+++ productservice/src/main/java/com/fullstackfamily/productservice/repository/ImageRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 @Repository
 public interface ImageRepository extends JpaRepository<Images, Long> {
     boolean existsByOriginalName(String originalName);
-    Optional<Images> findByOriginalName(String originalName);
+    Optional<Images> findByOriginalNameAndProduct_Sku(String originalName, String productSku);
 }
