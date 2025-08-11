f065fee4a00673bc86cca15c02533304b1786ffb
Lacky227 <tishko.artyr@gmail.com>
2025-08-11 15:48:05 +0300

    feat: add Spring Security config

---
+++ productservice/src/main/java/com/fullstackfamily/productservice/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.fullstackfamily.productservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(
+                            "/v3/api-docs/**",
+                            "/swagger-ui/**",
+                            "/swagger-ui.html")
+                            .permitAll();
+                    auth.anyRequest().authenticated();
+                }).build();
+    }
+}
