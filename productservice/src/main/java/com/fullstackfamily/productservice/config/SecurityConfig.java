1e776a7e07027169fb5bcdb7e4404cd466541c62
Lacky227 <tishko.artyr@gmail.com>
2025-08-11 16:17:20 +0300

    refactor: rename method

--- productservice/src/main/java/com/fullstackfamily/productservice/config/SecurityConfig.java
+++ productservice/src/main/java/com/fullstackfamily/productservice/config/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers(
