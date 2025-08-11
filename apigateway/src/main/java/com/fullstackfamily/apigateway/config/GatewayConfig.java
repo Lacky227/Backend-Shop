12e9952184230c1796b0868a383cbb735e3be8d2
Lacky227 <tishko.artyr@gmail.com>
2025-08-11 17:42:29 +0300

    feat: add AccessFilter and refine product-service-open routes

--- apigateway/src/main/java/com/fullstackfamily/apigateway/config/GatewayConfig.java
+++ apigateway/src/main/java/com/fullstackfamily/apigateway/config/GatewayConfig.java
@@ -1,5 +1,6 @@
 package com.fullstackfamily.apigateway.config;
 
+import com.fullstackfamily.apigateway.filter.AccessFilter;
 import com.fullstackfamily.apigateway.filter.JwtFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @AllArgsConstructor
 public class GatewayConfig {
     private final JwtFilter jwtFilter;
+    private final AccessFilter accessFilter;
 
     @Bean
     RouteLocator routes(RouteLocatorBuilder builder) {
@@ -25,14 +27,16 @@ public class GatewayConfig {
                         .uri("http://auth-service:8081/"))
 
                 .route("product-service-open", r -> r
-                        .path("/api/product/**")
+                        .path("/api/product/all",
+                                "/api/product/{sku}")
                         .uri("http://product-service:8082/"))
                 .route("product-service-protected", r -> r
                         .path("/api/files/save-image",
                                 "/api/product/create",
                                 "/api/product/delete/{sku}",
                                 "/api/product/update/{sku}")
-                        .filters(f -> f.filter(jwtFilter))
+                        .filters(f ->
+                                f.filter(jwtFilter).filter(accessFilter))
                         .uri("http://product-service:8082/"))
                 .route("product-swagger", r -> r
                         .path("/docs/product/**",
