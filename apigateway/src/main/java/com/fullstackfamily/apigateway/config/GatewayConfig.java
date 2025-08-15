fc812a1508cf0eae975331085aaecdf7b3dbd735
Lacky227 <tishko.artyr@gmail.com>
2025-08-15 17:38:07 +0300

    chore: reorder product-service routes to prioritize protected

--- apigateway/src/main/java/com/fullstackfamily/apigateway/config/GatewayConfig.java
+++ apigateway/src/main/java/com/fullstackfamily/apigateway/config/GatewayConfig.java
@@ -18,15 +18,15 @@ public class GatewayConfig {
                 .route("auth-service", r -> r
                         .path("/auth/**")
                         .uri("http://auth-service:8081/"))
-                .route("product-service-open", r -> r
-                        .path( "/api/product/**")
-                        .uri("http://product-service:8082/"))
                 .route("product-service-protected", r -> r
                         .path("/api/files/save-image",
                                 "/api/product/create",
                                 "/api/product/delete/**",
                                 "/api/product/update/**")
                         .filters(f -> f.filter(jwtFilter))
+                        .uri("http://product-service:8082/"))
+                .route("product-service-open", r -> r
+                        .path( "/api/product/**")
                         .uri("http://product-service:8082/"))
                 .route("notification-service", r -> r
                         .path("/api/notification/**")
